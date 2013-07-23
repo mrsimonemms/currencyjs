6ce97f643c7addfdf8152fb40883ff8c566fd534
RiggerTheGeek <simon@simonemms.com>
2013-07-23 09:10:35 +0100

    Got the library working with a MySQL database

--- test/conversionTest.js
+++ test/conversionTest.js
@@ -14,172 +14,172 @@ var Feed = require('../lib/model/Feed');
 var objConvert;
 
 describe('Convert between currencies', function() {
-    
-    
+
+
     it('should convert where both are base currency', function(done) {
-        
+
         /* Create the from object */
         var objFrom = new Feed({
             currency: 'EUR'
         });
-        
+
         /* Create the to object */
         var objTo = new Feed({
             currency: 'EUR'
         });
-        
+
         objConvert = new Convert(objFrom, objTo, 10.56);
-        
+
         expect(objConvert).to.be.instanceof(Convert);
-		expect(objConvert.getToValue()).to.be.eql(10.56);
+        expect(objConvert.getToValue()).to.be.eql(10.56);
         expect(objConvert.getToFormatted()).to.be.eql('10.56');
-		expect(objConvert.getFromValue()).to.be.eql(10.56);
+        expect(objConvert.getFromValue()).to.be.eql(10.56);
         expect(objConvert.getFromFormatted()).to.be.eql('10.56');
-        
+
         /* Calculate another value */
         objConvert.calculate(26);
-		expect(objConvert.getToValue()).to.be.eql(26);
+        expect(objConvert.getToValue()).to.be.eql(26);
         expect(objConvert.getToFormatted()).to.be.eql('26.00');
-		expect(objConvert.getFromValue()).to.be.eql(26);
+        expect(objConvert.getFromValue()).to.be.eql(26);
         expect(objConvert.getFromFormatted()).to.be.eql('26.00');
-        
+
         /* Calculate a reverse value */
         objConvert.calculateReverse(357890.994578);
-		expect(objConvert.getToValue()).to.be.eql(357890.994578);
+        expect(objConvert.getToValue()).to.be.eql(357890.994578);
         expect(objConvert.getToFormatted()).to.be.eql('357,890.99');
-		expect(objConvert.getFromValue()).to.be.eql(357890.994578);
+        expect(objConvert.getFromValue()).to.be.eql(357890.994578);
         expect(objConvert.getFromFormatted()).to.be.eql('357,890.99');
-        
+
         done();
-        
+
     });
-    
-    
+
+
     it('should convert values where one is base currency', function(done) {
-        
+
         /* Create the from object */
         var objFrom = new Feed({
             currency: 'GBP',
             base: 'EUR',
             rate: 1.16
         });
-        
+
         /* Create the to object */
         var objTo = new Feed({
             currency: 'EUR'
         });
-        
+
         objConvert = new Convert(objFrom, objTo, 22);
-        
+
         expect(objConvert).to.be.instanceof(Convert);
-		expect(objConvert.getToValue()).to.be.eql(25.52);
+        expect(objConvert.getToValue()).to.be.eql(25.52);
         expect(objConvert.getToFormatted()).to.be.eql('25.52');
-		expect(objConvert.getFromValue()).to.be.eql(22);
+        expect(objConvert.getFromValue()).to.be.eql(22);
         expect(objConvert.getFromFormatted()).to.be.eql('22.00');
-        
+
         /* Calculate another value */
         objConvert.calculate(6.96);
-		expect(objConvert.getToValue()).to.be.eql(8.0736);
+        expect(objConvert.getToValue()).to.be.eql(8.0736);
         expect(objConvert.getToFormatted()).to.be.eql('8.07');
-		expect(objConvert.getFromValue()).to.be.eql(6.96);
+        expect(objConvert.getFromValue()).to.be.eql(6.96);
         expect(objConvert.getFromFormatted()).to.be.eql('6.96');
-        
+
         /* Calculate a reverse value */
         objConvert.calculateReverse(8.0736);
-		expect(objConvert.getToValue()).to.be.eql(8.0736);
+        expect(objConvert.getToValue()).to.be.eql(8.0736);
         expect(objConvert.getToFormatted()).to.be.eql('8.07');
-		expect(objConvert.getFromValue()).to.be.eql(6.96);
+        expect(objConvert.getFromValue()).to.be.eql(6.96);
         expect(objConvert.getFromFormatted()).to.be.eql('6.96');
-        
+
         done();
-        
+
     });
-    
-    
+
+
     it('should convert values where neither is base currency', function(done) {
-        
+
         /* Create the from object */
         var objFrom = new Feed({
             currency: 'GBP',
             base: 'EUR',
             rate: 1.16
         });
-        
+
         /* Create the to object */
         var objTo = new Feed({
             currency: 'USD',
             base: 'EUR',
             rate: 0.78
         });
-        
+
         objConvert = new Convert(objFrom, objTo, 10);
-        
+
         expect(objConvert).to.be.instanceof(Convert);
-		expect(objConvert.getToValue()).to.be.eql(14.871794871794872);
+        expect(objConvert.getToValue()).to.be.eql(14.871794871794872);
         expect(objConvert.getToFormatted()).to.be.eql('14.87');
-		expect(objConvert.getFromValue()).to.be.eql(10);
+        expect(objConvert.getFromValue()).to.be.eql(10);
         expect(objConvert.getFromFormatted()).to.be.eql('10.00');
-        
+
         /* Calculate another value */
         objConvert.calculate(26);
-		expect(objConvert.getToValue()).to.be.eql(38.666666666666664);
+        expect(objConvert.getToValue()).to.be.eql(38.666666666666664);
         expect(objConvert.getToFormatted()).to.be.eql('38.67');
-		expect(objConvert.getFromValue()).to.be.eql(26);
+        expect(objConvert.getFromValue()).to.be.eql(26);
         expect(objConvert.getFromFormatted()).to.be.eql('26.00');
-        
+
         /* Calculate a reverse value */
         objConvert.calculateReverse(38.6666666666666); /* Limitation of the dependency - can only accept max. 15 digits */
-		expect(objConvert.getToValue()).to.be.eql(38.6666666666666);
+        expect(objConvert.getToValue()).to.be.eql(38.6666666666666);
         expect(objConvert.getToFormatted()).to.be.eql('38.67');
-		expect(objConvert.getFromValue()).to.be.eql(25.999999999999954);
+        expect(objConvert.getFromValue()).to.be.eql(25.999999999999954);
         expect(objConvert.getFromFormatted()).to.be.eql('26.00');
-        
+
         done();
-        
+
     });
-    
-    
+
+
     it('should convert values where both are same currency and non-base', function(done) {
-        
+
         /* Create the from object */
         var objFrom = new Feed({
             currency: 'GBP',
             base: 'EUR',
             rate: 1.16
         });
-        
+
         /* Create the to object */
         var objTo = new Feed({
             currency: 'GBP',
             base: 'EUR',
             rate: 1.16
         });
-        
+
         objConvert = new Convert(objFrom, objTo, 268.45);
-        
+
         expect(objConvert).to.be.instanceof(Convert);
-		expect(objConvert.getToValue()).to.be.eql(268.45);
+        expect(objConvert.getToValue()).to.be.eql(268.45);
         expect(objConvert.getToFormatted()).to.be.eql('268.45');
-		expect(objConvert.getFromValue()).to.be.eql(268.45);
+        expect(objConvert.getFromValue()).to.be.eql(268.45);
         expect(objConvert.getFromFormatted()).to.be.eql('268.45');
-        
+
         /* Calculate another value */
         objConvert.calculate(4578.45);
-		expect(objConvert.getToValue()).to.be.eql(4578.45);
+        expect(objConvert.getToValue()).to.be.eql(4578.45);
         expect(objConvert.getToFormatted()).to.be.eql('4,578.45');
-		expect(objConvert.getFromValue()).to.be.eql(4578.45);
+        expect(objConvert.getFromValue()).to.be.eql(4578.45);
         expect(objConvert.getFromFormatted()).to.be.eql('4,578.45');
-        
+
         /* Calculate a reverse value */
         objConvert.calculateReverse(5247);
-		expect(objConvert.getToValue()).to.be.eql(5247);
+        expect(objConvert.getToValue()).to.be.eql(5247);
         expect(objConvert.getToFormatted()).to.be.eql('5,247.00');
-		expect(objConvert.getFromValue()).to.be.eql(5247);
+        expect(objConvert.getFromValue()).to.be.eql(5247);
         expect(objConvert.getFromFormatted()).to.be.eql('5,247.00');
-        
+
         done();
-        
+
     });
-    
-    
+
+
 });
--- test/integrationTest.js
+++ test/integrationTest.js
@@ -2,14 +2,21 @@
  * Integration Test
  * 
  * Tests that require a database connection
- * to work
+ * to work.
+ * 
+ * To run these tests locally:
+ *  - copy TestConfig.travis.json to TestConfig.json
+ *  - change the database connection info as appropriate
+ *  - DO NOT COMMIT THIS FILE TO A PUBLIC REPOSITORY!!!
  * 
  * @author Simon Emms <simon@simonemms.com>
  */
+var dateformat = require('dateformat');
 var expect = require('chai').expect;
 
 var CurrencyJS = require('../lib/CurrencyJS');
 var Convert = require('../lib/model/Convert');
+var Feed = require('../lib/model/Feed');
 
 /* Fetch configuration */
 var objConfig = require('./TestConfig');
@@ -21,7 +28,7 @@ var objMySQL = objDbConfig.mysql || {};
 var objPGSQL = objDbConfig.postgresql || {};
 
 /* Define dates used in this test */
-var objNow = new Date();
+var objNow = new Date(dateformat(new Date(), 'yyyy-mm-dd'));
 var today = objNow.getDay();
 
 /* How many days ago was most recent weekday (yesterday or before) */
@@ -31,6 +38,8 @@ var objWeekday = new Date(objNow.getTime() - (weekday * 86400000));
 /* How many days ago was most recent weekend (yesterday or before) */
 var weekend = today === 0 ? 1 : today;
 var objWeekend = new Date(objNow.getTime() - (weekend * 86400000));
+
+var objWeekAgo = new Date(objWeekday.getTime() - (7 * 86400000));
 
 /* Declare currency object */
 var objCurrency;
@@ -70,7 +79,7 @@ describe('Tests on MySQL', function() {
             this.timeout(30000);
 
             objCurrency.import(function(err, importCount) {
-
+                
                 expect(err).to.be.null;
 
                 expect(importCount).to.be.a('number');
@@ -84,12 +93,188 @@ describe('Tests on MySQL', function() {
     
     });
     
-    /*
-    describe('converting currencies', function() {
+    
+    describe('currency conversion', function() {
         
-        it('should convert different currencies at current rate', function(done) {
+        it('should convert same currency', function(done) {
 
-            objCurrency.convert('USD', 'EUR', function(err, objOut) {
+            objCurrency.convert('USD', 'usd', function(err, objConvert) {
+                
+                expect(err).to.be.null;
+                
+                expect(objConvert).to.be.instanceof(Convert);
+                expect(objConvert).to.have.property('objFrom');
+                expect(objConvert.objFrom).to.be.instanceof(Feed);
+                expect(objConvert).to.have.property('objTo');
+                expect(objConvert.objTo).to.be.instanceof(Feed);
+                expect(objConvert.objFrom.date).to.be.eql(objConvert.objTo.date);
+                expect(objConvert).to.have.property('fromValue');
+                expect(objConvert.fromValue).to.be.a('number');
+                expect(objConvert).to.have.property('toValue');
+                expect(objConvert.toValue).to.be.a('number');
+                expect(objConvert).to.have.property('date');
+                expect(objConvert.date).to.be.instanceof(Date);
+                expect(objConvert).to.have.property('rate');
+                expect(objConvert.rate).to.be.an('object');
+                expect(objConvert).to.have.property('reverseRate');
+                expect(objConvert.reverseRate).to.be.an('object');
+                
+                done();
+
+            });
+
+        });
+        
+        it('should convert where from is base', function(done) {
+
+            objCurrency.convert('EUR', 'usd', function(err, objConvert) {
+                
+                expect(err).to.be.null;
+                
+                expect(objConvert).to.be.instanceof(Convert);
+                expect(objConvert).to.have.property('objFrom');
+                expect(objConvert.objFrom).to.be.instanceof(Feed);
+                expect(objConvert).to.have.property('objTo');
+                expect(objConvert.objTo).to.be.instanceof(Feed);
+                expect(objConvert.objFrom.date).to.be.eql(objConvert.objTo.date);
+                expect(objConvert).to.have.property('fromValue');
+                expect(objConvert.fromValue).to.be.a('number');
+                expect(objConvert).to.have.property('toValue');
+                expect(objConvert.toValue).to.be.a('number');
+                expect(objConvert).to.have.property('date');
+                expect(objConvert.date).to.be.instanceof(Date);
+                expect(objConvert).to.have.property('rate');
+                expect(objConvert.rate).to.be.an('object');
+                expect(objConvert).to.have.property('reverseRate');
+                expect(objConvert.reverseRate).to.be.an('object');
+                
+                done();
+
+            });
+
+        });
+        
+        it('should convert where to is base', function(done) {
+
+            objCurrency.convert('USD', 'eur', function(err, objConvert) {
+                
+                expect(err).to.be.null;
+                
+                expect(objConvert).to.be.instanceof(Convert);
+                expect(objConvert).to.have.property('objFrom');
+                expect(objConvert.objFrom).to.be.instanceof(Feed);
+                expect(objConvert).to.have.property('objTo');
+                expect(objConvert.objTo).to.be.instanceof(Feed);
+                expect(objConvert.objFrom.date).to.be.eql(objConvert.objTo.date);
+                expect(objConvert).to.have.property('fromValue');
+                expect(objConvert.fromValue).to.be.a('number');
+                expect(objConvert).to.have.property('toValue');
+                expect(objConvert.toValue).to.be.a('number');
+                expect(objConvert).to.have.property('date');
+                expect(objConvert.date).to.be.instanceof(Date);
+                expect(objConvert).to.have.property('rate');
+                expect(objConvert.rate).to.be.an('object');
+                expect(objConvert).to.have.property('reverseRate');
+                expect(objConvert.reverseRate).to.be.an('object');
+                
+                done();
+
+            });
+
+        });
+        
+        it('should receive options with weekday', function(done) {
+            
+            var objOptions = {
+                date: objWeekday,
+                value: 6.32
+            };
+
+            objCurrency.convert('usd', 'GBP', objOptions, function(err, objConvert) {
+                
+                expect(err).to.be.null;
+                
+                expect(objConvert).to.be.instanceof(Convert);
+                expect(objConvert).to.have.property('objFrom');
+                expect(objConvert.objFrom).to.be.instanceof(Feed);
+                expect(objConvert).to.have.property('objTo');
+                expect(objConvert.objTo).to.be.instanceof(Feed);
+                expect(objConvert.objFrom.date).to.be.eql(objConvert.objTo.date);
+                expect(objConvert).to.have.property('fromValue');
+                expect(objConvert.fromValue).to.be.a('number').to.be.eql(6.32);
+                expect(objConvert).to.have.property('toValue');
+                expect(objConvert.toValue).to.be.a('number');
+                expect(objConvert).to.have.property('date');
+                expect(objConvert.date).to.be.instanceof(Date);
+                expect(dateformat(objConvert.date, 'yyyy-mm-dd')).to.be.eql(dateformat(objWeekday, 'yyyy-mm-dd'));
+                expect(objConvert).to.have.property('rate');
+                expect(objConvert.rate).to.be.an('object');
+                expect(objConvert).to.have.property('reverseRate');
+                expect(objConvert.reverseRate).to.be.an('object');
+                
+                done();
+
+            });
+
+        });
+        
+        it('should receive options with weekend day', function(done) {
+            
+            var objOptions = {
+                date: objWeekend,
+                value: 12.00
+            };
+
+            objCurrency.convert('usd', 'GBP', objOptions, function(err, objConvert) {
+                
+                expect(err).to.be.null;
+                
+                expect(objConvert).to.be.instanceof(Convert);
+                expect(objConvert).to.have.property('objFrom');
+                expect(objConvert.objFrom).to.be.instanceof(Feed);
+                expect(objConvert).to.have.property('objTo');
+                expect(objConvert.objTo).to.be.instanceof(Feed);
+                expect(objConvert.objFrom.date).to.be.eql(objConvert.objTo.date);
+                expect(objConvert).to.have.property('fromValue');
+                expect(objConvert.fromValue).to.be.a('number').to.be.eql(12);
+                expect(objConvert).to.have.property('toValue');
+                expect(objConvert.toValue).to.be.a('number');
+                expect(objConvert).to.have.property('date');
+                expect(objConvert.date).to.be.instanceof(Date);
+                expect(dateformat(objConvert.date, 'yyyy-mm-dd'));
+                expect(objConvert).to.have.property('rate');
+                expect(objConvert.rate).to.be.an('object');
+                expect(objConvert).to.have.property('reverseRate');
+                expect(objConvert.reverseRate).to.be.an('object');
+                
+                /**
+                 * For weekend dates, it gets the last possible
+                 * weekday. This may not be a Friday - eg, Good
+                 * Friday is a holiday.  Set allowable range to
+                 * be one week
+                 */
+                var validDate = objConvert.date >= objWeekAgo && objConvert.date <= objNow;
+                
+                expect(validDate).to.be.true;
+                
+                done();
+
+            });
+
+        });
+        
+    });
+        
+    describe('error handling', function() {
+
+        it('should handle From Currency error', function(done) {
+
+            objCurrency.convert('fail', 'GBP', function(err, objConvert) {
+
+                expect(err).to.be.instanceof(Error);
+                expect(err.message).to.be.eql('Invalid "from" currency');
+
+                expect(objConvert).to.be.null;
 
                 done();
 
@@ -97,16 +282,81 @@ describe('Tests on MySQL', function() {
 
         });
 
-        it('should throw errors when not set up correctly', function(done) {
+        it('should handle To Currency error', function(done) {
 
-            objCurrency.convert('currency', 'rubbish', function(err, objOut) {
+            objCurrency.convert('USD', 'fail', function(err, objConvert) {
+
+                expect(err).to.be.instanceof(Error);
+                expect(err.message).to.be.eql('Invalid "to" currency');
+
+                expect(objConvert).to.be.null;
 
                 done();
 
             });
 
         });
-        
-    });*/
+
+        it('should handle From Currency error with to as Base Currency', function(done) {
+
+            objCurrency.convert('fail', 'EUR', function(err, objConvert) {
+
+                expect(err).to.be.instanceof(Error);
+                expect(err.message).to.be.eql('Invalid "from" currency');
+
+                expect(objConvert).to.be.null;
+
+                done();
+
+            });
+
+        });
+
+        it('should handle To Currency error with from as Base Currency', function(done) {
+
+            objCurrency.convert('EUR', 'fail', function(err, objConvert) {
+
+                expect(err).to.be.instanceof(Error);
+                expect(err.message).to.be.eql('Invalid "to" currency');
+
+                expect(objConvert).to.be.null;
+
+                done();
+
+            });
+
+        });
+
+        it('should handle date errors', function(done) {
+
+            objCurrency.convert('gbp', 'GBP', {date: 'invalid date'}, function(err, objConvert) {
+
+                expect(err).to.be.instanceof(Error);
+                expect(err.message).to.be.eql('Invalid date given - must be a date object');
+
+                expect(objConvert).to.be.null;
+
+                done();
+
+            });
+
+        });
+
+        it('should handle value errors', function(done) {
+
+            objCurrency.convert('eur', 'eur', {value: 'invalid value'}, function(err, objConvert) {
+
+                expect(err).to.be.instanceof(Error);
+                expect(err.message).to.be.eql('Invalid value given - must be a number');
+
+                expect(objConvert).to.be.null;
+
+                done();
+
+            });
+
+        });
+
+    });
     
 });
